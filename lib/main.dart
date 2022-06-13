b150ee2e07393ce95379104df0a62395ed25ed02
josoyun <dbsdk1408@naver.com>
2022-06-14 01:55:23 +0900

    calculate layout

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Simple Calculator'),
     );
   }
 }
@@ -48,16 +48,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _number = 0;
 
-  void _incrementCounter() {
+
+  void _clear() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _number = 0;
     });
   }
 
@@ -70,46 +71,180 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: const Center(
+                  child: Text('For display'),
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 5,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("CE"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("C"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("DEL"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("/"),
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("7"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("8"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("9"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("*"),
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("4"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("5"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("6"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("-"),
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("1"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("2"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("3"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("+"),
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("+/-"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("0"),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("."),
+                              )
+                          ),
+                          Expanded(
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("="),
+                              )
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+            )
+          ],
+        )
     );
   }
 }
